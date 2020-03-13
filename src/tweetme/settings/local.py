c7b575b9c1e91b9b0769b163ced77dcd0b95029e
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2020-03-14 01:30:47 +0530

    path for static files set

--- src/tweetme/settings/local.py
+++ src/tweetme/settings/local.py
@@ -117,3 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static-storage"),
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-serve")
