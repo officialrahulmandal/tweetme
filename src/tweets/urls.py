96e8822bfa5c4e2b8259b221d7a3b92ddd73a9ed
rahul kumar mandal <rahul.mandal@toprankers.co.in>
2020-03-15 18:17:13 +0530

    class based views

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import tweet_detail_view, tweet_list_view
+from .views import tweet_detail_view, tweet_list_view, TweetDetailView, TweetListView
 
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('', tweet_list_view, name='list'),
-    path('/1/', tweet_detail_view, name='detail'),
+    path('', TweetListView.as_view(), name='list'),
+    path('/1/', TweetDetailView.as_view(), name='detail'),
 ]
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
 from .models import Tweet
+
+class TweetDetailView(DetailView):
+    queryset = Tweet.objects.all()
+
+    def get_object(self):
+        return Tweet.objects.get(id=1)
+
+class TweetListView(ListView):
+    queryset = Tweet.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TweetListView, self).get_context_data(*args, **kwargs)
+        return context
+
 
 # Create your views here.
 def tweet_detail_view(request, id=1):
