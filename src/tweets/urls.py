b9a11f0f87583ad755cf8a016a9ae1fe108e22c3
rahul kumar mandal <rahul.mandal@toprankers.co.in>
2020-03-15 16:19:58 +0530

    CRUD first view

---
+++ src/tweets/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import tweet_detail_view, tweet_list_view
+
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('', tweet_list_view, name='list'),
+    path('1/', tweet_detail_view, name='detail'),
+]
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def tweet_detail_view(request, id=1):
+    return render(request, "tweets/tweet_detail_view.html", {})
+
+def tweet_list_view(request, id=1):
+    return render(request,"tweets/tweet_list_view.html", {})
