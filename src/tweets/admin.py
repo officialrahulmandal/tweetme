43a021a0ab3f9e560fb72765bbf0cfb6e2fd5f3b
rahul kumar mandal <rahul.mandal@toprankers.co.in>
2020-03-19 13:51:03 +0530

    validators

--- src/tweets/admin.py
+++ src/tweets/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Tweet
+from .forms import TweetModelForm
 
-admin.site.register(Tweet)
+#admin.site.register(Tweet)
+
+class TweetModelAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Tweet
+        form = TweetModelForm
+
+admin.site.register(Tweet, TweetModelAdmin)
--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.db import models
+from .validators import validate_content
 
 # Create your models here.
+
 
 class Tweet(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
     on_delete=models.CASCADE)
-    content  = models.CharField(max_length=140)
+    content  = models.CharField(max_length=140, validators=[validate_content])
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     
